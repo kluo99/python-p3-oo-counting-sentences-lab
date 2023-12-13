cb79ce1674262caa8ef292b99986e054e85e9190
Kenneth <luo.kenneth1999@gmail.com>
2023-12-13 11:12:35 -0500

    finished lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,44 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value = '') -> None:
+      self.value = value
+
+  @property
+  def value(self):
+      return self._value
+  
+  @value.setter
+                #can usually set to anything butpeople ussually set to the same name
+  def value(self, value):
+      if type(value) == str:
+          self._value = value
+      else:
+          print("The value must be a string.")
+  
+  def is_sentence(self):
+      last_character = self.value[-1]
+
+      # return last_character == '.'
+      return self.value.endswith('.')
+  
+  def is_question(self):
+      last_character = self.value[-1]
+
+      # return last_character == '.'
+      return self.value.endswith('?')
+
+  def is_exclamation(self):
+      last_character = self.value[-1]
+
+      # return last_character == '.'
+      return self.value.endswith('!')\
+  
+  def count_sentences(self):
+      sentence = 0
+      prev_character = ''
+      for c in self.value:
+          if c in ['.', '!', '?'] and prev_character.isalpha() :
+              sentence += 1
+          prev_character = c
+      return sentence
